379abafad3314075b2b7cf37e952a7a913b4a320
ngarg <ngarg@slb.com>
2021-02-26 19:08:36 -0800

    zshrc

---
+++ setup.sh
@@ -0,0 +1,11 @@
+
+cp tmux.conf ~/.tmux.conf
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+cp zshrc ~/.zshrc
+
+
+
+
